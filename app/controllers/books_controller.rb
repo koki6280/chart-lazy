9159122f778b9177ccc871a78432b7314a425947
vagrant <vagrant@localhost.localdomain>
2020-06-16 04:08:39 +0000

    start[5-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+	before_action :authenticate_user!
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  	@user = current_user
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  	@user = current_user
+  end
+
+  def new
+  end
+
+  def create
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+    if  @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book.id)
+    else
+    	@books = Book.all
+    	flash.now[:alert] = 'error'
+    	render action: :index
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if  @book.update(book_params)
+  		flash[:notice] = "Book was successfully updated."
+  	    redirect_to book_path(@book)
+  	else
+  		flash.now[:alert] = 'error'
+        render action: :edit
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	if book.destroy
+  		flash[:notice] = "You have updated book successfully."
+        redirect_to books_path(@books)
+    else
+    	render action: :new
+    end
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4f132b912ccbc671b8a8785c7e4bd9790202de995b6486ee2bef17ecd52a5fa0c17c64ca8dd3b2c15690fad42ebd65ec547f82dfccc0e93cedc51fbc45980263'
